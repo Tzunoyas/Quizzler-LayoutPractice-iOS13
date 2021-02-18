0c5c837e488081dd39ece655e80db7a4f047c1aa
Razvan Pricop <razvanipricop@gmail.com>
2021-02-18 22:53:17 +0200

    First commit with basic functionality in place

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by razvan.pricop on 17/02/2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
